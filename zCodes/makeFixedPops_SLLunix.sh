362e9af4533d2381666f130f0825262433d5c3c3
Shane L. Larson <68617046+shane-science@users.noreply.github.com>
2026-05-13 10:33:13 -0600

    Commented out NS lines; just WD runs for this file
    From CALLISTO, Clarkson Topaz 3.6.1; for GVSU (I'm at LISA Sprint in Bozeman)

--- zCodes/makeFixedPops_SLLunix.sh
+++ zCodes/makeFixedPops_SLLunix.sh
@@ -358,7 +358,7 @@ echo
 echo "now working on NS+WD: 13 and 10,11,12..."
 (( duration1 = $SECONDS ))            ## time at start of population run
 
-cosmic-pop --final-kstar1 13 --final-kstar2 10 12 --inifile ../buildFiles/thin_Params.ini --Nstep 10000 --Niter 1000000000 -n 12 >> ../errLogs/log01_Fixed.log 2>> ../errLogs/err01_Fixed.log < /dev/null
+# cosmic-pop --final-kstar1 13 --final-kstar2 10 12 --inifile ../buildFiles/thin_Params.ini --Nstep 10000 --Niter 1000000000 -n 12 >> ../errLogs/log01_Fixed.log 2>> ../errLogs/err01_Fixed.log < /dev/null
 
 (( duration2 = $SECONDS - duration1))            ## time at end of population run and echo to screen
 echo "   RUNTIME NS+WD = $(($duration2/86400)) DAY $((($duration2 % 86400)/3600)) HR $(((($duration2 % 86400)%3600)/60)) MIN $(((($duration2 % 86400)%3600)%60)) SEC"
@@ -379,7 +379,7 @@ echo
 echo "now working on NS+NS: 13 and 13..."
 (( duration1 = $SECONDS ))            ## time at start of population run
 
-cosmic-pop --final-kstar1 13 --final-kstar2 13 --inifile ../buildFiles/thin_Params.ini --Nstep 10000 --Niter 1000000000 -n 12 >> ../errLogs/log01_Fixed.log 2>> ../errLogs/err01_Fixed.log < /dev/null
+# cosmic-pop --final-kstar1 13 --final-kstar2 13 --inifile ../buildFiles/thin_Params.ini --Nstep 10000 --Niter 1000000000 -n 12 >> ../errLogs/log01_Fixed.log 2>> ../errLogs/err01_Fixed.log < /dev/null
 
 (( duration2 = $SECONDS - duration1))            ## time at end of population run and echo to screen
 echo "   RUNTIME NS+NS = $(($duration2/86400)) DAY $((($duration2 % 86400)/3600)) HR $(((($duration2 % 86400)%3600)/60)) MIN $(((($duration2 % 86400)%3600)%60)) SEC"
@@ -390,7 +390,7 @@ echo
 echo "now working on BH+NS: 14 and 13..."
 (( duration1 = $SECONDS ))            ## time at start of population run
 
-cosmic-pop --final-kstar1 14 --final-kstar2 13 --inifile ../buildFiles/thin_Params.ini --Nstep 10000 --Niter 1000000000 -n 12 >> ../errLogs/log01_Fixed.log 2>> ../errLogs/err01_Fixed.log < /dev/null
+# cosmic-pop --final-kstar1 14 --final-kstar2 13 --inifile ../buildFiles/thin_Params.ini --Nstep 10000 --Niter 1000000000 -n 12 >> ../errLogs/log01_Fixed.log 2>> ../errLogs/err01_Fixed.log < /dev/null
 
 (( duration2 = $SECONDS - duration1))            ## time at end of population run and echo to screen
 echo "   RUNTIME NS+NS = $(($duration2/86400)) DAY $((($duration2 % 86400)/3600)) HR $(((($duration2 % 86400)%3600)/60)) MIN $(((($duration2 % 86400)%3600)%60)) SEC"
